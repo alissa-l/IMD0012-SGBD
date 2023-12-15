7c610d0b0a8fb94e4844fe053394b9165d546813
mariana-raquel <marianaquel.morais@gmail.com>
2023-12-14 22:57:31 -0300

    add apagar registro

--- apagar_registro.c
+++ apagar_registro.c
@@ -1,32 +1,56 @@
 #include "common_utils/dependecies.h"
 
-void apagar_registro() {
+void apagar_registro_tabela(Tabela tab, int id) {
+    FILE *arquivoOriginal, *arquivoNovo;
 
+    char nomeDiretorio[200] = "tabelas/";
+    strcat(nomeDiretorio, tab.nome);
+    strcat(nomeDiretorio, ".pwn");
+    char ch;
+    int temp = 0;
+    arquivoOriginal = fopen(nomeDiretorio, "r");
+    arquivoNovo = fopen("tabelas/temp.pwn", "w");
+
+    if(arquivoOriginal == NULL) {
+        print_vermelho("Erro na abertura do arquivo\n");
+    } else {
+        while(feof(arquivoOriginal) == 0) {
+            char line[1000];
+            fscanf(arquivoOriginal, "%s\n", line);
+            char *pedaco = strtok(line, ";");
+            if(strcmp(pedaco, id) != 0) {
+                fprintf(arquivoOriginal, "%s\n", line);
+            }
+        }
+        fclose(arquivoOriginal);
+        fclose(arquivoNovo);
+    }
+    remove(nomeDiretorio);
+    rename("tabelas/temp.pwn", nomeDiretorio);
+    print_verde("Registro apagado com sucesso!");
+}
+
+void apagar_registro() {
     ListaTabela listaTabelas = listar_tabelas(true);
     int ind = -1;
     while(ind < 0) {
         char nome[20];
         printf("De qual tabela você deseja apagar o registro? Informe o nome da tabela:\n");
         scanf("%s", nome);
-
         for(int i = 0; i < listaTabelas.qtdTabelas; i++) {
             if(strcmp(listaTabelas.tabelas[i].nome, nome) == 0) {
                 ind = i;
                 break;
             }
         }
-
         if(ind < 0) {
             print_vermelho("Por favor, digite um nome de tabela válido!");
         }
     }
 
     Tabela tab = listaTabelas.tabelas[ind];
-
     printf("%s - %i\n", tab.nome, tab.qtdColunas);
-
     int qtdRegistros = get_ultimo_registro(tab.nome);
-    printf("%i\n", qtdRegistros);
     if(qtdRegistros > 0) {
         listar_dados_tabela(tab);
         int id = -1;
@@ -38,17 +62,10 @@ void apagar_registro() {
             }
         }
 
-        /**
-        
-            APAGAR REGISTRO DO ID FORNECIDO!
-
-        */
-
-
-
+        apagar_registro_tabela(tab, id);
+       
     } else {
         print_vermelho("Não há dados cadastrados nessa tabela!\n");
     }
-
 
 }
